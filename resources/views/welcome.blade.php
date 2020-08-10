07658c96d8e221f3ba86b572ee806ef26294f680
Miroslav Orincak <info@joomla-webstranky.sk>
2020-08-10 23:09:24 +0200

    text in articles listing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,10 @@
 
     <ul>
         @foreach($posts as $post)
-            <li><h3>{{$post->title}}</h3></li>
+            <li>
+                <h3>{{$post->title}}</h3>
+                <p>{{$post->text}}</p>
+            </li>
         @endforeach
     </ul>
 
