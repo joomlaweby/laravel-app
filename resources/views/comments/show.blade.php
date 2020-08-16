6e1c3890ee10f3e0b5af8e2e026b89c9c75431ef
Miroslav Orincak <info@joomla-webstranky.sk>
2020-08-16 12:22:48 +0200

    datetime attribute added to time element

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -6,7 +6,7 @@
         <a href="/users/{{ $comment->user->id }}" class="author">
             <strong>{{ $comment->user->name }}</strong>
         </a>
-        <time class="text-muted">
+        <time datetime="{{ $comment->created_at->toW3cString() }}" class="text-muted">
             {{ $comment->created_at->diffForHumans() }}
         </time>
     </div>
