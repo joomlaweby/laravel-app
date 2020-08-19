2f6c8e88506bcca3dba8edd4a8d1dbdf2c606938
Miroslav Orincak <info@joomla-webstranky.sk>
2020-08-19 21:59:45 +0200

    comments form added for logged in user

---
+++ resources/views/comments/create.blade.php
@@ -0,0 +1,5 @@
+<form action="" method="POST" class="">
+    <label>Comment</label>
+    <textarea name="text" class="uk-textarea uk-margin-bottom" rows="5"></textarea>
+    <button class="uk-button uk-button-primary" type="submit">Submit</button>
+</form>
