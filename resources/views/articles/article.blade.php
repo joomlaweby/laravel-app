e62d64f202b097afa6dc95df272ea0de4be48eb0
Miroslav Orincak <info@joomla-webstranky.sk>
2020-09-06 13:56:13 +0200

    slugs for articles urls

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -1,14 +1,14 @@
 <article class="uk-article uk-margin-bottom">
-    <h2 class="uk-article-title"><a href="/articles/{{ $article->id }}">{{$article->title}}</a></h2>
+    <h2 class="uk-article-title"><a href="/articles/{{ $article->slug }}">{{$article->title}}</a></h2>
     <p class="uk-article-meta">Written by <a href="#">{{$article->user->name}}</a> on {{ $article->created_at->format('d/m/Y') }}. Posted in <a href="#">Blog</a></p>
     <div class="uk-margin-bottom">{{$article->text}}</div>
     <div class="uk-grid-small uk-child-width-auto" uk-grid>
 
         <div>
-            <a class="uk-button uk-button-text" href="/articles/{{ $article->id }}">Read more</a>
+            <a class="uk-button uk-button-text" href="/articles/{{ $article->slug }}">Read more</a>
         </div>
         <div>
-            <a class="uk-button uk-button-text" href="/articles/{{ $article->id }}">{{$article->comments->count()}} {{ str_plural('comment' , $article->comments->count()) }}</a>
+            <a class="uk-button uk-button-text" href="/articles/{{ $article->slug }}">{{$article->comments->count()}} {{ str_plural('comment' , $article->comments->count()) }}</a>
         </div>
     </div>
 </article>
