31d359c238a7d320198be69f8efc7a4f7a8289f5
Miroslav Orincak <info@joomla-webstranky.sk>
2020-09-06 22:02:33 +0200

    users list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
-    function profile() {
+    public function index()
+    {
+        return view('users.index', [
+            'title' => 'Users',
+            'users' => User::latest('id')->limit(50)->get()
+        ]);
+
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', [
+            'title' => 'User profile',
+            'user' => $user
+        ]);
+    }
+
+    function profile(User $user) {
         return view('userprofile', [
-            'title' => 'User profile'
+            'title' => 'User profile',
+            'user' => $user
         ]);
     }
 }
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="content">
+        <div class="title m-b-md">
+            <h1>{{$title}}</h1>
+        </div>
+        <table class="uk-table uk-table-divider">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>
+                        {{$user->id}}
+                    </td>
+                    <td>
+                        <a href="/users/{{ $user->id }}">{{$user->name}}</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
+
+    </div>
+@endsection
