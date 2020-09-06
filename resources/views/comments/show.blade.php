667e0e1ddfe7f1e1b7e45ec72a902f6dc2275588
Miroslav Orincak <info@joomla-webstranky.sk>
2020-09-07 00:06:33 +0200

    uikit for comment template

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,15 +1,23 @@
 <content-comment inline-template>
-    <article class="comment">
-        <div class="content">
-            {{ $comment->text }}
-        </div>
-        <div>
-            <a href="/users/{{ $comment->user->id }}" class="author">
-                <strong>{{ $comment->user->name }}</strong>
-            </a>
-            <time datetime="{{ $comment->created_at->toW3cString() }}" class="text-muted">
-                {{ $comment->created_at->diffForHumans() }}
-            </time>
+    <article class="uk-comment">
+        <header class="uk-comment-header">
+            <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                <div class="uk-width-auto">
+                    <img class="uk-comment-avatar" src="images/avatar.jpg" width="80" height="80" alt="">
+                </div>
+                <div class="uk-width-expand">
+                    <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="/users/{{ $comment->user->id }}">{{ $comment->user->name }}</a></h4>
+                    <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
+                        <li><time datetime="{{ $comment->created_at->toW3cString() }}" class="text-muted">
+                                {{ $comment->created_at->diffForHumans() }}
+                            </time></li>
+                        <li><a href="#">Reply</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <div class="uk-comment-body">
+            <p>{{ $comment->text }}</p>
         </div>
     </article>
 </content-comment>
