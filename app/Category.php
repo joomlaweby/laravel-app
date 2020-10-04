457db7e65114c97ad8b3854b866da86c69831c98
Miroslav Orincak <info@joomla-webstranky.sk>
2020-10-04 12:32:56 +0200

    category model functions

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function getRouteKeyName () {
+        return 'slug';
+    }
+
+    public function articles() {
+        return $this->hasMany('App\Article')->latest();
+    }
 }
