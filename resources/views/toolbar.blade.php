531414239b133e8f97831eba14f04026a5d70c03
Miroslav Orincak <info@joomla-webstranky.sk>
2020-10-04 22:42:13 +0200

    toolbar new link fixed

--- resources/views/toolbar.blade.php
+++ resources/views/toolbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="uk-navbar-container" uk-navbar>
     <div class="uk-navbar-left">
         <ul class="uk-navbar-nav">
-            <li class="uk-active "><a href="/{{ Request::segment(1) }}/create" class="uk-button uk-button-small uk-button-primary">New</a></li>
+            <li class="uk-active "><a href="/@if(Request::segment(1) != 'admin'){{ Request::segment(1) }}@else()admin/{{ Request::segment(2) }}@endif/create" class="uk-button uk-button-small uk-button-primary">New</a></li>
             <li class="uk-parent"><a href="" class="uk-button uk-button-small uk-button-default">Publish</a></li>
             <li class="uk-parent"><a href="" class="uk-button uk-button-small uk-button-default">Unpublish</a></li>
             <li class="uk-parent"><a href="" class="uk-button uk-button-small uk-button-default">Delete</a></li>
