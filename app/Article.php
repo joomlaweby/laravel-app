7c63e7397c7f8f83a1ee6b662e0ae43c31eec136
Miroslav Orincak <info@joomla-webstranky.sk>
2020-10-04 14:05:14 +0200

    file doc comments

--- app/Article.php
+++ app/Article.php
@@ -15,18 +15,30 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Article extends Model
 {
+    /**
+     * @return string
+     */
     public function getRouteKeyName () {
         return 'slug';
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user() {
         return $this->belongsTo('App\User');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function comments() {
         return $this->hasMany('App\Comment')->latest();
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function category() {
         return $this->belongsTo('App\Category');
     }
