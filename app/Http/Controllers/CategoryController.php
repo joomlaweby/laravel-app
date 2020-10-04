498e8baa50e6bf214bb5db54dc8f9920f8c80a0c
Miroslav Orincak <info@joomla-webstranky.sk>
2020-10-04 12:30:26 +0200

    category controller index function

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    public function index()
+    {
+        return view('categories.index', [
+            'title' => 'Welcome',
+            'categories' => Category::all()->latest('id')->limit(50)->get()
+        ]);
+
+    }
+
     public function show(Category $category)
     {
 
