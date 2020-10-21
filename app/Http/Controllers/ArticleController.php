2f0bfa70e956ea1cb0883d60a9549d308fee376d
Miroslav Orincak <info@joomla-webstranky.sk>
2020-10-21 23:35:49 +0200

    constructors removed from controllers

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
 
+
     /**
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
