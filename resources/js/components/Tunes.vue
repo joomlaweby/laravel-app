c8b7fdca3228ba388bef56b6ccf0d866877778a7
Miroslav Orincak <info@joomla-webstranky.sk>
2020-12-05 18:48:42 +0100

    Tunes.vue component

---
+++ resources/js/components/Tunes.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    methods: {
+        getMusic() {
+            axios.get('/user?ID=12345')
+                .then(function (response) {
+                    // handle success
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+        }
+    }
+}
+</script>
