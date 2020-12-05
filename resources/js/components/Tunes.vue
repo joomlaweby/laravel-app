b55ca678911ea44b407e1cd9b21cdd91078b5006
Miroslav Orincak <info@joomla-webstranky.sk>
2020-12-05 20:47:30 +0100

    Tunes data rendered in view

--- resources/js/components/Tunes.vue
+++ resources/js/components/Tunes.vue
@@ -1,7 +1,16 @@
 <template>
-    <form action="#" @submit.prevent="getMusic()">
-        <input class="uk-input" placeholder="search for song" v-model="query" type="text">
-    </form>
+
+
+    <div>
+        <form action="#" @submit.prevent="getMusic()">
+            <input class="uk-input" placeholder="search for song" v-model="query" type="text">
+        </form>
+
+        <div v-for="song in songs" :key="song.id">
+            <h2>{{ song.name }}</h2>
+            <p><strong>{{ song.artist }}</strong> - {{ song.album }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,20 +19,36 @@ import axios from "axios";
 export default {
     data() {
         return {
-            query: ''
+            query: '',
+            songs: []
         }
     },
     methods: {
         getMusic() {
-            axios.get(`https://itunes.apple.com/search?term=${encodeURI(this.query)}&entity=musicTrack&limit=15`)
+            axios.get(`https://itunes.apple.com/search?term=${encodeURI(this.query)}&entity=musicTrack&limit=5`)
                 .then(response => {
                     // handle success
-                    console.log(response);
+
+                    response.data.results.forEach(song => {
+                        if (song.kind == 'song') {
+                            this.songs.push(this.extractData(song));
+                        }
+                    })
                 })
                 .catch(function (error) {
                     // handle error
                     console.log(error);
                 })
+        },
+        extractData({
+                        artistName: artist,
+                        trackName: name,
+                        trackId: id,
+                        previewUrl: audioFile,
+                        artworkUrl100: cover,
+                        collectionName: album
+                    }) {
+            return {artist, name, id, audioFile, cover, album};
         }
     }
 }
