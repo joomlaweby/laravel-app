4a807ead0da8f10d5d4cb1213ce07584e0db2516
Miroslav Orincak <info@joomla-webstranky.sk>
2020-12-06 18:19:34 +0100

    song card template tweaked

--- resources/js/components/TunesListSong.vue
+++ resources/js/components/TunesListSong.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="uk-card uk-card-primary uk-card-default uk-card-body uk-card-hover">
-        <div class="uk-card-media-top">
-            <img :src="song.cover" alt="">
-        </div>
-        <h2 class="uk-card-title">{{ song.name }}</h2>
-        <p><strong>{{ song.artist }}</strong> - {{ song.album }}</p>
-        <div>
-            <figure v-if="song.audioFile">
-                <audio
-                    controls
-                    :src="song.audioFile">
-                    Your browser does not support the
-                    <code>audio</code> element.
-                </audio>
-            </figure>
+    <div>
+        <div class="uk-card uk-card-default uk-card-body uk-card-hover">
+            <div class="uk-card-media-top">
+                <img :src="song.cover" alt="">
+            </div>
+            <h2 class="uk-card-title">{{ song.name }}</h2>
+            <h4 class="uk-margin-remove">{{ song.artist }}</h4>
+            <p>{{ song.album }}</p>
+            <div>
+                <figure v-if="song.audioFile">
+                    <audio
+                        controls
+                        :src="song.audioFile">
+                        Your browser does not support the
+                        <code>audio</code> element.
+                    </audio>
+                </figure>
+            </div>
         </div>
     </div>
+
 </template>
 
 <script>
