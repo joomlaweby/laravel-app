d49f3c7899f1a8b5d92f4592e5dbb1fc57f44d4b
Miroslav Orincak <info@joomla-webstranky.sk>
2020-12-13 23:55:31 +0100

    admin navigation and view content

--- resources/js/admin/Admin.vue
+++ resources/js/admin/Admin.vue
@@ -1,11 +1,20 @@
 <template>
-<div>hello</div>
+    <div>
+        <nav>
+            <router-link to="/admin/">Dashboard</router-link>
+            <router-link to="/admin/users">Users</router-link>
+            <router-link to="/admin/comments">Comments</router-link>
+            <router-link to="/admin/articles">Articles</router-link>
+            <router-link to="/admin/categories">Categories</router-link>
+        </nav>
+        <div>
+            <router-view />
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
--- resources/js/admin/router.js
+++ resources/js/admin/router.js
@@ -1,6 +1,10 @@
 import Router from "vue-router";
 import Vue from 'vue'
 import Dashboard from './views/Dashboard'
+import Articles from './views/Articles'
+import Comments from './views/Comments'
+import Users from './views/Users'
+import Categories from './views/Categories'
 Vue.use(Router)
 
 export default new Router({
@@ -9,7 +13,22 @@ export default new Router({
         {
             path: '/admin/',
             component: Dashboard
+        },
+        {
+            path: '/admin/articles',
+            component: Articles
+        },
+        {
+            path: '/admin/comments',
+            component: Comments
+        },
+        {
+            path: '/admin/users',
+            component: Users
+        },
+        {
+            path: '/admin/categories',
+            component: Categories
         }
-
     ]
 })
